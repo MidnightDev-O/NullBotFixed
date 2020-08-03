70660a7e3c9746ed40c7d24a93270cd4ade5dfea
ExtraordinaryDev <61729264+ExtraordinaryDev@users.noreply.github.com>
2020-08-03 06:15:38 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,17 @@ client.on('ready', () => {
     console.log("Online");
 });
 
+client.on('guildMemberAdd', member => {
+    // Send the message to a designated channel on a server:
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'join-logs');
+    // Do nothing if the channel wasn't found on this server
+    if (!channel) return;
+    // Send the message, mentioning the member
+    channel.send(`Welcome to the server, ${member}`);
+  });
+  
+
+
 //______________________________________________4 commands here
 //COMMANDS
 
