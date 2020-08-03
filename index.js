2b39e7ca4421fa275e088dab583c9daf90a61d14
ExtraordinaryDev <61729264+ExtraordinaryDev@users.noreply.github.com>
2020-08-03 06:11:32 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = "^";
 
-
-
-
 client.login(process.env.token);
+
+client.on('ready', () => {
+    client.user.setActivity("^help || nullbyte");
+    console.log("Online");
+});
+
+client.on('message', msg => {
+    if (msg.content === `${prefix}` + "Test") {
+        msg.channel.send("Complete")
+    }
+});
